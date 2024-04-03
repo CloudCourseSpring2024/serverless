8998f27b5714cfad550bcc82b7e826e15dd22a0c
HasiniMuvva <muvva.h@northeastern.edu>
2024-04-03 01:21:43 -0400

    serverless port removed and changed to https

--- ccfunc/index.js
+++ ccfunc/index.js
@@ -9,7 +9,7 @@ const sendVerificationEmail = async (data) => {
     from: 'webapp account creation <no-reply@mg.spring2024cc.me>',
     to: data.username,
     subject: 'Verify your email address',
-    text: `Click the following link to verify your email address: http://spring2024cc.me:3000/verify/${data.id}`,
+    text: `Click the following link to verify your email address: https://spring2024cc.me/verify/${data.id}`,
   };
 
   try {
@@ -25,7 +25,13 @@ const sendVerificationEmail = async (data) => {
 export const handleNewUser = async (data, context) => {
   try {
     const userData = JSON.parse(Buffer.from(data.data, 'base64').toString());
+
+    // Send verification email
     await sendVerificationEmail(userData);
+
+    // Track email in database
+    // await trackEmailsInDatabase(userData);
+
     console.log('Verification email sent and tracking data stored successfully.');
   } catch (error) {
     console.error('Error handling new user:', error);
